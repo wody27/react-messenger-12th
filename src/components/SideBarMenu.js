993e42826b388351c2e57ba24ac9d6d3717458d9
jaeyong <maxwodyd98@gmail.com>
2020-10-02 15:14:14 +0900

    update: 사이드바 아이콘 추가

--- src/components/SideBarMenu.js
+++ src/components/SideBarMenu.js
@@ -5,16 +5,53 @@ import { Link } from 'react-router-dom';
 export default function SideBarMenu() {
   return (
     <Wrapper>
-      <Link to="/friend">친구</Link>
-      <Link to="/chat">채팅</Link>
-      <Link to="/setting">설정</Link>
+      <TopIcons>
+        <Link to="/friend">
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892448-9b8e7000-04bf-11eb-8d14-fba287d101be.jpg"
+            alt="친구"
+          />
+        </Link>
+        <Link to="/chat">
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892455-a0532400-04bf-11eb-8e6d-5c50ca79640e.png"
+            alt="채팅"
+          />
+        </Link>
+        <Link to="/setting">
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892451-9e896080-04bf-11eb-9e3e-35f3d5c72ee5.jpg"
+            alt="설정"
+          />
+        </Link>
+      </TopIcons>
+      <BottomIcons>
+        <Button>
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892452-9f21f700-04bf-11eb-83ac-1b59cc80d4b1.png"
+            alt="캘린더"
+          />
+        </Button>
+        <Button>
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892453-9f21f700-04bf-11eb-8dbe-70d3d6e2f832.png"
+            alt="알람"
+          />
+        </Button>
+        <Button>
+          <Img
+            src="https://user-images.githubusercontent.com/56102421/94892454-9fba8d80-04bf-11eb-8cfb-198dc358a8d4.png"
+            alt="설정"
+          />
+        </Button>
+      </BottomIcons>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  height: 100%;
   width: 70px;
+  height: 100%;
 
   position: fixed;
   top: 0;
@@ -25,5 +62,33 @@ const Wrapper = styled.div`
 
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
+`;
+
+const TopIcons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const BottomIcons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin: 0;
+  padding: 0;
+  color: red;
+`;
+const Button = styled.button`
+  width: 30px;
+  height: 30px;
+
+  margin: 0;
+  padding: 0;
 `;
